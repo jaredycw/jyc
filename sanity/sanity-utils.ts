340828c957e15b59ece9f6a8c91083e26078f926
Jared Yeung <130919853+jaredycw@users.noreply.github.com>
2024-08-30 11:15:36 +0800

    Update sanity-utils.ts

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -294,7 +294,7 @@ export async function getWorks(): Promise<Work[]>{
 
     return createClient(clientConfig).fetch(
 
-        groq`*[_type=="work"][0..10]| order(postOrder desc){
+        groq`*[_type=="work"][0..9]| order(postOrder desc){
             _id,
             "slug": slug.current,
             title,"mainImage":mainImage.asset->url,
@@ -370,7 +370,7 @@ export async function getExps(): Promise<Experiment[]>{
 
     return createClient(clientConfig).fetch(
 
-        groq`*[_type=="experiment"][0..10]| order(postOrder desc){
+        groq`*[_type=="experiment"][0..9]| order(postOrder desc){
             _id,
             "slug": slug.current,
             title,"mainImage":mainImage.asset->url,
